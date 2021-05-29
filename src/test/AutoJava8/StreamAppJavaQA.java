5669a34dc42be2348abf80562e4d553ba70c3787
abhinay5993 <abhinaylunawat5993@gmail.com>
2021-05-29 23:33:07 +0530

    pushing to local repo

---
+++ src/test/AutoJava8/StreamAppJavaQA.java
@@ -0,0 +1,9 @@
+package test.AutoJava8;
+
+public class StreamAppJavaQA {
+	
+	public static void main(String[] args) {
+		System.out.println("\nThis is for working with Java 8 latest features!!!..");
+	}
+
+}
