acca4f3035f4505b1daedde6415d699fb23add75
abhinay5993 <abhinaylunawat5993@gmail.com>
2021-05-30 16:19:53 +0530

    commitig to git by adding new file

---
+++ TestJava8ExpsApp/src/test/AutoJava8/LplQaSdet.java
@@ -0,0 +1,9 @@
+package test.AutoJava8;
+
+class LplQaSdet {
+	public static void main(String name[]) {
+		System.out.println("\nHey!! There welcome to GIt branch!!...");
+		
+		System.out.println("git Test with Java 8");
+	}
+}
