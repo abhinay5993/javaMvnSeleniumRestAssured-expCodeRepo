67716e054634815eefab5e2c31f01cb8e63b720e
abhinay5993 <abhinaylunawat5993@gmail.com>
2021-05-30 16:34:06 +0530

    pushing whole codebase to developed branch

---
+++ TestExpSeleniumUIFrameWorks/src/test/java/testScripts/StreamAppJavaQA.java
@@ -0,0 +1,9 @@
+package testScripts;
+
+public class StreamAppJavaQA {
+	
+	public static void main(String[] args) {
+		System.out.println("\nThis is for working with Java 8 latest features!!!..");
+	}
+
+}
