577f3a2da72ec016ce54db1497c4ee9f0abc785b
AbhinayL <abhinaylunawat5993@gmail.com>
2021-05-31 00:45:42 +0530

    adding it for amazon UI codeBase
    adding it for amazon UI codeBase

--- TestJava8ExpsApp/src/test/AutoJava8/LplQaSdet.java
+++
@@ -1,9 +0,0 @@
-package test.AutoJava8;
-
-class LplQaSdet {
-	public static void main(String name[]) {
-		System.out.println("\nHey!! There welcome to GIt branch!!...");
-		
-		System.out.println("git Test with Java 8");
-	}
-}
