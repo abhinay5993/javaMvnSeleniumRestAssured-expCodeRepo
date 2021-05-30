06b2e40eae35780bd82565a11adf496b5d8c4996
abhinay5993 <abhinaylunawat5993@gmail.com>
2021-05-30 16:40:39 +0530

    adding new pack to frameWork and adding new pojo

---
+++ TestJava8ExpsApp/src/test/pojo/ResponseRootCl.java
@@ -0,0 +1,23 @@
+package test.pojo;
+
+public class ResponseRootCl {
+
+	private String status;
+	private String statusMessage;
+	private String empId;
+	private double salary;
+	
+	public String getStatus() {
+		return status;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	
+}
