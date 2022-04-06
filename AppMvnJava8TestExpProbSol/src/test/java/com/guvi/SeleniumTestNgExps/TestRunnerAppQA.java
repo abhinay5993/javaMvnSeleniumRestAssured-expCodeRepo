e5edbbd021da2296de3ad7f9c9be0b8c1f3d5642
abhinay5993 <abhinaylunawat5993@gmail.com>
2022-04-06 17:32:12 +0530

    java assignment problem solve from guvi till assignment11

--- AppMvnJava8TestExpProbSol/src/test/java/com/guvi/SeleniumTestNgExps/TestRunnerAppQA.java
+++ AppMvnJava8TestExpProbSol/src/test/java/com/guvi/SeleniumTestNgExps/TestRunnerAppQA.java
@@ -1,9 +1,7 @@
 package com.guvi.SeleniumTestNgExps;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
