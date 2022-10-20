3de56e2389f1184454cb925a3f57f8d5522726f3
abhinay5993 <abhinaylunawat5993@gmail.com>
2022-10-21 02:34:56 +0530

    adding problem solving with Loops,Patters etc

--- AppMvnJava8TestExpProbSol/src/main/java/com/acciojob/preDSA/DiamondPattern.java
+++ AppMvnJava8TestExpProbSol/src/main/java/com/acciojob/preDSA/DiamondPattern.java
@@ -9,25 +9,25 @@ public class DiamondPattern {
 		int tc=sc.nextInt();
 		int n = sc.nextInt();
 		
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i < n; i++) {
 
 			for (int j = n; j > i; j--) {
 				System.out.print(" ");
 			}
 
-			for (int k = 1; k <=(2 * i - 1); k++) {
+			for (int k = 0; k <(2 * i - 1); k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
-		for (int i = n-1; i>=1; i--) {
+		for (int i = n-3; i>=1; i--) {
 
 			for (int j = n; j > i; j--) {
 				System.out.print(" ");
 			}
 
-			for (int k = 1; k <=(2 * i - 1); k++) {
+			for (int k =0; k <(2 * i - 1); k++) {
 				System.out.print("*");
 			}
 			System.out.println();
