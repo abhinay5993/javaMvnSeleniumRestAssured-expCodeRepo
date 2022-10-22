ef7587848a6f3fa70b35dbd0f394b3c8ff66d870
abhinay5993 <abhinaylunawat5993@gmail.com>
2022-10-22 20:52:17 +0530

    completed odd diamond pattern with While/for loops

--- AppMvnJava8TestExpProbSol/src/main/java/com/acciojob/preDSA/DiamondPattern.java
+++ AppMvnJava8TestExpProbSol/src/main/java/com/acciojob/preDSA/DiamondPattern.java
@@ -2,35 +2,35 @@ package com.acciojob.preDSA;
 import java.util.Scanner;
 
 public class DiamondPattern {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		int tc=sc.nextInt();
-		int n = sc.nextInt();
-		
-		for (int i = 1; i < n; i++) {
+		int t = sc.nextInt();
 
-			for (int j = n; j > i; j--) {
-				System.out.print(" ");
+		while (t-- > 0) {
+			
+			int n = sc.nextInt();
+			for (int i = 1; i <= (n / 2) + 1; i++) {
+				for (int j = 1; j <= (n - (2 * i - 1)); j++) {
+					System.out.print(" ");
+				}
+				for (int j = 1; j <= (i * 2) - 1; j++) {
+					System.out.print("* ");
+				}
+				System.out.println();
 			}
 
-			for (int k = 0; k <(2 * i - 1); k++) {
-				System.out.print("*");
+			for (int i = n / 2; i >= 1; i--) {
+				for (int j = 1; j <= (n - (2 * i - 1)); j++) {
+					System.out.print(" ");
+				}
+				for (int j = 1; j <= (i * 2) - 1; j++) {
+					System.out.print("* ");
+				}
+				System.out.println();
 			}
-			System.out.println();
-		}
-		
-		for (int i = n-3; i>=1; i--) {
-
-			for (int j = n; j > i; j--) {
-				System.out.print(" ");
-			}
-
-			for (int k =0; k <(2 * i - 1); k++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			
 		}
 		
 	}
