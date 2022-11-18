318e342f2fd46e139496081cbd49526a24d16b8b
abhinay5993 <abhinaylunawat5993@gmail.com>
2022-11-19 01:02:25 +0530

    string problems solves from AccoicoJobs Nov-Module

---
+++ AppMvnJava8TestExpProbSol/src/main/java/com/acciojob/subArrsStrings/AutoReStrTransform.java
@@ -0,0 +1,18 @@
+package com.acciojob.subArrsStrings;
+import java.util.Scanner;
+
+public class AutoReStrTransform {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.println("\nEnter the String : ");
+		String inpStr=scn.nextLine();
+		System.out.println("\nOutput Str : "+printAutoReStrTransform(inpStr));
+	}
+
+	private static String printAutoReStrTransform(String inpStr) {
+		
+		return inpStr;
+	}
+
+}
