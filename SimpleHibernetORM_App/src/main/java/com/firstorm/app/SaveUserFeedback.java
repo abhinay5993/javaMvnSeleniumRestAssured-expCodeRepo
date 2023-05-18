06db2d59c95053f25cda3509b11254ff4ae59089
Abhinay Lunawat <abhinaylunawat5993@gmail.com>
2023-05-18 20:26:55 +0530

    sample insert data to table using hibernet with .hbm.xml file example

--- SimpleHibernetORM_App/src/main/java/com/firstorm/app/SaveUserFeedback.java
+++ SimpleHibernetORM_App/src/main/java/com/firstorm/app/SaveUserFeedback.java
@@ -1,13 +1,37 @@
 package com.firstorm.app;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import com.firstorm.feedpojo.CustomerFeedbackDetails;
+import com.github.javafaker.Faker;
 
-/**
- * Hello world!
- *
- */
-public class SaveUserFeedback 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class SaveUserFeedback {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Faker fkg=new Faker();
+		String emailData=fkg.name().firstName()+"@gmail.com";
+		String feedMsg=fkg.dragonBall().character();
+		int rateCount=fkg.number().numberBetween(0,10);
+		
+		//Persistance Object
+		CustomerFeedbackDetails cusFeedObj=new CustomerFeedbackDetails();
+		cusFeedObj.setUserEmail(emailData);
+		cusFeedObj.setFeedBackMsg(feedMsg);
+		cusFeedObj.setRateCount(rateCount);
+		
+		Configuration configObj = new Configuration();
+		configObj.configure("hibernate.cfg.xml");
+		
+		SessionFactory sf = configObj.buildSessionFactory();
+		Session ses = sf.openSession();
+		Transaction t1 = ses.beginTransaction();
+		
+		ses.save(cusFeedObj);
+		t1.commit();
+		System.out.println("\nFeedBack!! data saved to table..");
+		ses.close();
+	}
+	
 }
